cbe9505d2b5f5dad3c156a414e536b66a8050e51
akmaurya288 <akmaurya288@gmail.com>
2019-02-21 22:05:28 +0530

    First

--- lib/main.dart
+++ lib/main.dart
@@ -19,64 +19,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-//
-//class Firstscreen extends StatefulWidget {
-//  @override
-//  _FirstscreenState createState() => _FirstscreenState(100);
-//
-//}
-//
-//class _FirstscreenState extends State<Firstscreen> {
-//  double siz;
-//  _FirstscreenState(this.siz);
-//  @override
-//  void initState() {
-//    Timer(Duration(seconds: 5),()=>_FirstscreenState(200));
-//    // TODO: implement initState
-//    super.initState();
-//  }
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      body: Stack(
-//        fit: StackFit.expand,
-//          children: <Widget>[
-//            Container(
-//              decoration: BoxDecoration(color: Color(0xffb71c1c)),
-//            ),
-//            Column(
-//              mainAxisAlignment: MainAxisAlignment.start,
-//              children: <Widget>[
-//                Expanded(flex: 2 ,child: Container(
-//                  child: Column(
-//                    mainAxisAlignment: MainAxisAlignment.center,
-//                    children: <Widget>[
-//                      CircleAvatar(
-//                        backgroundColor: Colors.white,
-//                        radius: siz,
-//                        child: Image.asset('lib/Icon/IconCarR.png'),
-//                      ),
-//                      Padding(padding: EdgeInsets.only(top: 10.0)),
-//                      Text("DriverTest",style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),)
-//                    ],
-//                  ),
-//                )
-//                ),
-//                Expanded(flex:1,child: Column(
-//                  mainAxisAlignment: MainAxisAlignment.center,
-//                  children: <Widget>[
-//                    CircularProgressIndicator(),
-//
-//                  ],
-//                ))
-//              ],
-//            )
-//          ],
-//      ),
-//    );
-//  }
-//}
-
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -197,3 +139,60 @@ class MainPage extends StatelessWidget {
     );
   }
 }
+//
+//class Firstscreen extends StatefulWidget {
+//  @override
+//  _FirstscreenState createState() => _FirstscreenState(100);
+//
+//}
+//
+//class _FirstscreenState extends State<Firstscreen> {
+//  double siz;
+//  _FirstscreenState(this.siz);
+//  @override
+//  void initState() {
+//    Timer(Duration(seconds: 5),()=>_FirstscreenState(200));
+//    // TODO: implement initState
+//    super.initState();
+//  }
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      body: Stack(
+//        fit: StackFit.expand,
+//          children: <Widget>[
+//            Container(
+//              decoration: BoxDecoration(color: Color(0xffb71c1c)),
+//            ),
+//            Column(
+//              mainAxisAlignment: MainAxisAlignment.start,
+//              children: <Widget>[
+//                Expanded(flex: 2 ,child: Container(
+//                  child: Column(
+//                    mainAxisAlignment: MainAxisAlignment.center,
+//                    children: <Widget>[
+//                      CircleAvatar(
+//                        backgroundColor: Colors.white,
+//                        radius: siz,
+//                        child: Image.asset('lib/Icon/IconCarR.png'),
+//                      ),
+//                      Padding(padding: EdgeInsets.only(top: 10.0)),
+//                      Text("DriverTest",style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),)
+//                    ],
+//                  ),
+//                )
+//                ),
+//                Expanded(flex:1,child: Column(
+//                  mainAxisAlignment: MainAxisAlignment.center,
+//                  children: <Widget>[
+//                    CircularProgressIndicator(),
+//
+//                  ],
+//                ))
+//              ],
+//            )
+//          ],
+//      ),
+//    );
+//  }
+//}
